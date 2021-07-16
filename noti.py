11e746675a6f3dc61e95f08bebbb01e67ac06bed
Mulie23 <47581954+Mulie23@users.noreply.github.com>
2021-07-16 11:25:17 +0800

    update

--- noti.py
+++ noti.py
@@ -1,6 +1,6 @@
 import requests 
 
-header = {"jwt" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIxIiwiaWF0IjoxNjI2MjMzNDAyLCJleHAiOjE2MjYzMTk4MDJ9._HjrM5XbmKZbMATpCZ2_1w3hbWpfQm2-svLTOxW7AAE"}
+header = {"jwt" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIxIiwiaWF0IjoxNjI2NDA0OTAxLCJleHAiOjE2MjY0OTEzMDF9.mj-K-OZij9k_fRzt0GWvvyTrt95ygvzMnKKjAD7FYgY"}
 body = {"medboxID": "1","username": "user1"}
 
 response3 = requests.post("http://3.0.17.207:4000/notification/send", body, headers=header)
