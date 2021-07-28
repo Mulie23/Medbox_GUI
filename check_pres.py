36296c2c725ceca75aa8feb591fbb57d7f1e63fe
Mulie23 <47581954+Mulie23@users.noreply.github.com>
2021-07-28 15:10:41 +0800

    update

--- check_pres.py
+++ check_pres.py
@@ -1,7 +1,7 @@
 import requests 
 import json
 
-header = {"jwt" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIxIiwiaWF0IjoxNjI2ODQyNjY1LCJleHAiOjE2MjY5MjkwNjV9.L_bOISzaIMUGM9d0L0dbGjFQt_tHmf4ZQ1Rl-Lo1GDY"}
+header = {"jwt" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIxIiwiaWF0IjoxNjI3NDU1MzM4LCJleHAiOjE2Mjc1NDE3Mzh9.YPddmGSFu-M0RD-_x5zQHdVOW2JLSVPP_2oAhxLGehk"}
 body = {"medboxID": "1","username": "user1"}
 
 response3 = requests.post("http://3.0.17.207:4000/queue/consume", body, headers=header)
