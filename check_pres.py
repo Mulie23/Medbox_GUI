abb63833303beee8e324a0bf2ad654dc17050bb2
Mulie23 <47581954+Mulie23@users.noreply.github.com>
2021-07-28 14:53:21 +0800

    update

--- check_pres.py
+++ check_pres.py
@@ -1,8 +1,11 @@
 import requests 
+import json
 
 header = {"jwt" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIxIiwiaWF0IjoxNjI2ODQyNjY1LCJleHAiOjE2MjY5MjkwNjV9.L_bOISzaIMUGM9d0L0dbGjFQt_tHmf4ZQ1Rl-Lo1GDY"}
 body = {"medboxID": "1","username": "user1"}
 
 response3 = requests.post("http://3.0.17.207:4000/queue/consume", body, headers=header)
 data = response3.json()
+with open ("prescription,json","w") as f:
+    json.dump(data,f)
 print(data)
