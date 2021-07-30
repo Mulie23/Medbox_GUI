92f649e4bc0400a4629fa4d7f583375eb2249722
Mulie23 <47581954+Mulie23@users.noreply.github.com>
2021-07-30 14:14:04 +0800

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -89,6 +89,7 @@ class Containers() :
                         medicine_name_in_pres = i["medicine_name"]
                         message_in_pres = i["message"]
                 refilling_quantity.value=medicine_name_in_pres
+                print(refilling_quantity)
                 container_id  = self.unfilled_containers.pop(0) 
                 self.data[container_id]["filled"] = 1 
                 self.data[container_id]["quantity_left"] = 0 
@@ -472,6 +473,7 @@ def confirm_finish():
     with open("container.json") as f:
         data = json.load(f)
     for i in data:
+        print(data[i]["medicine"]["name"])
         if data[i]["medicine"]["name"] == refilling_quantity.value:
             data[i]["quantity_left"] += quantity_no.value
     with open("container.json","w") as f:
