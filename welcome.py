9a12d171260275f1f63ff9dee6b03c9b4c357b97
Mulie23 <47581954+Mulie23@users.noreply.github.com>
2021-08-05 22:06:58 +0800

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -239,7 +239,47 @@ def dispense(med_id, qty):
     container.writeToFile()    
     return True
 
+def refillProcess() : 
+    # pull updated prescription 
+    container = Containers(DIR, STEP, SLEEP) 
+    stateMachine = True ; 
+    state = 'barcode'
+    while(stateMachine) : 
+        if (state=="barcode") : 
+            quantity_window.show(wait=True)
+            refill_window.hide()
+            #display the relevant details on the front end - Wentao
+            # information on what medicine are to be filled up 
+            medicine_id = checkBarcode() ; 
+            if medicine_id!=None : 
+                state = "rotate"
 
+            # add GUI interrupt 
+        elif (state=="rotate") : 
+            container_id = container.getContainer(medicine_id)
+            if container.rotateContainerToRefillArea(container_id) : 
+                state="finish"
+            else : 
+                state = "error"
+                message = "couldn't rotate container"
+        # elif (state=="wait") : 
+        #     # wait for a button push on gui and number of pills form input 
+        #     # update infromation i.e container.json
+        #     if refillComplete() : 
+        #         state = "finish"
+        #     else : 
+        #         state = "barcode" 
+        elif (state=="finish"): 
+            container.writeToFile() 
+            stateMachine = False 
+#            refill_window.show(wait=True)
+        elif  (state=="error") : 
+            error = "there was some error" 
+            stateMachine = False 
+        else : 
+            state = "error" 
+            error = "invalid sate" 
+    return  True
 
 def refillComplete() : 
     # return true if all the medicines have been refilled 
