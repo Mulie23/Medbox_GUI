8cb07385a3a7f15e0012f8baaaf3f061cb58a585
Mulie23 <47581954+Mulie23@users.noreply.github.com>
2021-08-14 16:51:21 +0800

    update

--- gui_test.py
+++
@@ -1,8 +0,0 @@
-from guizero import App, Combo, Text, CheckBox, ButtonGroup, PushButton, info, TextBox, Picture, Slider, Window, info, Box
-app = App(title="My second GUI app")
-list1 = ["asd","asdaads","asasdasd"]
-if len(list1)<10:
-    for i in range(10-len(list1)):
-        list1.append("")
-combo1 = Combo(app, options=[list1[0],list1[1],list1[2],list1[3]])
-app.display()
